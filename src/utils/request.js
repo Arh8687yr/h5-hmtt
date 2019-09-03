ed13c51ab721fba6041012cee70a6e8013b82232
执念 <1250696730@qq.com>
2019-09-03 14:33:54 +0800

    配置大数字

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,22 @@
 import axios from 'axios'
-// import JSONbig from 'json-bigint'
+import JSONbig from 'json-bigint'
 
 const instance = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/app/'
+  baseURL: 'http://ttapi.research.itcast.cn'
 })
 
 // 大数字处理
+// 获取到服务器返回的数据，并且是在处理数据之前使用
+// transformResponse 是在响应拦截器之前执行
+instance.defaults.transformResponse = [function (data) {
+  try {
+    // data 数据可能不是标准的 JSON 格式字符串，否则会导致 JSONbig.parse(data) 转换失败报错
+    return JSONbig.parse(data)
+  } catch (err) {
+    // 无法转换的数据直接原样返回
+    return data
+  }
+}]
 
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
