cf07ccd95f7febcce1c9aac48d357a308b6846fb
执念 <1250696730@qq.com>
2019-09-05 21:45:41 +0800

    使用Tabbar组件定义底部导航及子路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Tabbar from '@/views/Tabbar'
+import Home from '@/views/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      component: Tabbar,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: Home
+        },
+        {
+          path: '/ask',
+          name: 'ask',
+          component: () => import('@/views/Home/Ask.vue')
+        },
+        {
+          path: '/video',
+          name: 'video',
+          component: () => import('@/views/Home/Video.vue')
+        },
+        {
+          path: '/user',
+          name: 'user',
+          component: () => import('@/views/Home/User.vue')
+        }
+      ]
+    },
     {
       path: '/login',
       name: 'login',
