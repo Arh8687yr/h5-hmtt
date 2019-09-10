d01fe53068694239e16803a1e4a65f71475f911b
执念 <1250696730@qq.com>
2019-09-10 10:09:16 +0800

    文章详情页面美化

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-    <!-- 头部导航 -->
-    <van-nav-bar title="文章详情" left-text="返回" fixed left-arrow @click-left="$router.back()" />
-    <div class="article" v-if="article">
-      <!-- 文章标题 -->
-      <h2 class="article-title">{{article.title}}</h2>
-      <!-- 作者信息 -->
-      <!-- 文章内容 -->
-      <div class="article-content" v-html="article.content">
+  <div class="article-container">
+    <van-nav-bar fixed left-text="返回" @click-left="$router.back()" title="文章详情"></van-nav-bar>
+    <div class="detail" v-if="article">
+      <h3 class="title">{{article.title}}</h3>
+      <div class="author">
+        <van-image round width="2rem" height="2rem" fit="fill" :src="article.aut_photo" />
+        <div class="text">
+          <p class="name">{{article.aut_name}}</p>
+          <p class="time">{{article.pubdate}}</p>
+        </div>
+        <van-button round size="small" type="info">+ 关注</van-button>
       </div>
-      <!-- 点赞和取消 -->
+      <div class="content" v-html="article.content"></div>
+      <div class="zan">
+        <van-button round size="small" hairline type="primary" plain icon="good-job-o">点赞</van-button>&nbsp;&nbsp;&nbsp;&nbsp;
+        <van-button round size="small" hairline type="danger" plain icon="delete">不喜欢</van-button>
+      </div>
     </div>
   </div>
 </template>
@@ -41,16 +47,57 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
-.article {
-  margin-top: 92px;
-  padding: 0px 20px;
-  .article-title {
-    font-size: 40px;
-    font-weight: bold;
+<style scoped lang='less'>
+.article-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  overflow-y: scroll;
+  width: 100%;
+  height: 100%;
+}
+.article-loading {
+  padding-top: 60px;
+  text-align: center;
+}
+.error {
+  padding-top: 100px;
+  text-align: center;
+}
+.detail {
+  padding: 50px 10px;
+  .title {
+    font-size: 16px;
   }
-  .article-content {
-    font-size: 26px;
+  .zan {
+    text-align: center;
+  }
+  .author {
+    padding: 10px 0;
+    display: flex;
+    .text {
+      flex: 1;
+      padding-left: 10px;
+      line-height: 1.3;
+      .name {
+        font-size: 14px;
+        margin: 0;
+      }
+      .time {
+        margin: 0;
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+  .content {
+    overflow: hidden;
+    white-space: pre-wrap;
+    word-break: break-all;
+    /deep/ img {
+      max-width: 100%;
+      background: #f9f9f9;
+    }
   }
 }
 </style>
